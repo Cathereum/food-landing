a3ff1741e19a9ce10fea23258a7dfb159c7d1485
Cathereum <106606021+Cathereum@users.noreply.github.com>
2023-02-02 19:10:35 +0700

    did a timer

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 window.addEventListener("DOMContentLoaded", () => {
+  //Tabs
   const tabsList = document.querySelector(".tabheader__items");
   const tabs = document.querySelectorAll(".tabheader__item");
   const tabsContent = document.querySelectorAll(".tabcontent");
@@ -32,6 +33,50 @@ window.addEventListener("DOMContentLoaded", () => {
     }
   });
 
+  //Timer
+
+  const deadLine = "2023-02-04";
+
+  const getTimeRemaining = (endTime) => {
+    const t = Date.parse(endTime) - Date.parse(new Date());
+
+    const days = Math.floor(t / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+    const minutes = Math.floor((t / 1000 / 60) % 60);
+    const seconds = Math.floor((t / 1000) % 60);
+
+    return {
+      total: t,
+      days: days,
+      hours: hours,
+      minutes: minutes,
+      seconds: seconds,
+    };
+  };
+
+  const setClock = (selector, endTime) => {
+    const timer = document.querySelector(selector);
+    const days = timer.querySelector("#days");
+    const hours = timer.querySelector("#hours");
+    const minutes = timer.querySelector("#minutes");
+    const seconds = timer.querySelector("#seconds");
+
+    const updateClock = () => {
+      const time = getTimeRemaining(endTime);
+
+      days.innerHTML = time.days;
+      hours.innerHTML = time.hours;
+      minutes.innerHTML = time.minutes;
+      seconds.innerHTML = time.seconds;
+
+      if (time.total <= 0) {
+        clearInterval(timeInterval);
+      }
+    };
+    const timeInterval = setInterval(updateClock, 1000);
+  };
+
   hideTabContent();
   showTabs();
+  setClock(".timer", deadLine);
 });
