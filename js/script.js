a5a8c9fbc81eb9cb026cc828dc34738e580652a4
Cathereum <106606021+Cathereum@users.noreply.github.com>
2023-02-06 13:41:05 +0700

    menu render from js completed

--- js/script.js
+++ js/script.js
@@ -35,7 +35,7 @@ window.addEventListener("DOMContentLoaded", () => {
 
   //Timer
 
-  const deadLine = "2023-02-04";
+  const deadLine = "2024-02-04";
 
   const getTimeRemaining = (endTime) => {
     const t = Date.parse(endTime) - Date.parse(new Date());
@@ -84,22 +84,22 @@ window.addEventListener("DOMContentLoaded", () => {
   const modal = document.querySelector(".modal");
   const modalCloseBtn = modal.querySelector("[data-close]");
 
-  modalTrigger.forEach((btn) => {
-    btn.addEventListener("click", (event) => {
-      if (event.target) {
-        // modal.classList.add("show");
-        modal.classList.toggle("show");
-
-        document.body.style.overflow = "hidden";
-      }
-    });
-  });
+  const openModal = () => {
+    // modal.classList.add("show");
+    modal.classList.toggle("show");
+    document.body.style.overflow = "hidden";
+    clearInterval(autoOpenModal);
+  };
 
   const closeModal = () => {
     //   modal.classList.remove("show");
     modal.classList.toggle("show");
     document.body.style.overflow = "";
   };
+
+  modalTrigger.forEach((btn) => {
+    btn.addEventListener("click", openModal);
+  });
 
   modalCloseBtn.addEventListener("click", closeModal);
 
@@ -115,6 +115,70 @@ window.addEventListener("DOMContentLoaded", () => {
       console.log("ESC");
     }
   });
+
+  //   const autoOpenModal = setInterval(openModal, 10000);
+
+  //Menu
+
+  class MenuCard {
+    constructor(src, alt, title, desc, price, parentSelector) {
+      this.src = src;
+      this.alt = alt;
+      this.title = title;
+      this.desc = desc;
+      this.price = price;
+      this.parent = document.querySelector(parentSelector);
+      this.transfer = 70;
+      this.changeToRUB();
+    }
+
+    changeToRUB() {
+      this.price = this.price * this.transfer;
+    }
+
+    render() {
+      const element = document.createElement("div");
+      element.innerHTML = `
+      <div class="menu__item">
+        <img src=${this.src} alt=${this.alt}>
+        <h3 class="menu__item-subtitle">Меню ${this.title}</h3>
+        <div class="menu__item-descr">${this.desc}</div>
+        <div class="menu__item-divider"></div>
+        <div class="menu__item-price">
+          <div class="menu__item-cost">Цена:</div>
+          <div class="menu__item-total"><span>${this.price}</span> руб/день</div>
+        </div>
+    </div>`;
+      this.parent.append(element);
+    }
+  }
+
+  new MenuCard(
+    "img/tabs/vegy.jpg",
+    "vegy",
+    '"Фитнес"',
+    " Меню 'Фитнес' - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!",
+    9,
+    ".menu .container"
+  ).render();
+
+  new MenuCard(
+    "img/tabs/elite.jpg",
+    "elite",
+    "“Премиум”",
+    "В меню 'Премиум' мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан! ",
+    10,
+    ".menu .container"
+  ).render();
+
+  new MenuCard(
+    "img/tabs/post.jpg",
+    "post",
+    '"Постное"',
+    "Меню 'Постное' - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.",
+    12,
+    ".menu .container"
+  ).render();
 
   hideTabContent();
   showTabs();
